42eab7c12b15939793be2726387b22ab1d470a90
Aidan Ng <aidanng@aidans-air-8.mynetworksettings.com>
2024-09-01 16:42:05 -0400

    added addBet and createDuels

--- frontend/app/database.tsx
+++ frontend/app/database.tsx
@@ -374,3 +374,73 @@ export const getGroupCode = async (groupID: string): Promise<string | undefined>
          return undefined;
     }
 }
+
+//BET FUNCTIONS
+
+//create head to head 
+export const createDuels = async (firstUserID: string, secondUserID: string, groupID: string): Promise<string | undefined> => {
+    try {
+
+        if (!firstUserID || firstUserID.trim() === '') {
+            throw new Error('Invalid userID for player one');
+        }
+        else if(!secondUserID || secondUserID.trim() === ''){
+            throw new Error('Invalid userID for player two');
+        }
+
+        const groupDocRef = doc(db, 'groups', groupID)
+
+        const duelRef = await addDoc(collection(groupDocRef, 'duels'), {
+            "players": {
+                [firstUserID]: {
+                    "steps": 0,
+                },
+                [secondUserID]: {
+                    "steps": 0,
+                }
+            },
+            "betters": {
+
+            },
+            "createdAt": serverTimestamp(),
+            "updatedAt": serverTimestamp(),
+            "groupImageUrl": null,
+        })
+        const duelID = duelRef.id;
+        console.log("createDuel - response:", duelID);
+        return duelID
+
+
+
+    } catch (error) {
+        console.error("createDuel - Error fetching user document:", error);
+        return undefined;
+    }
+}
+
+//add bet
+
+export const addBet = async (userID: string, wager: number, duelID: string, groupID: string,): Promise<undefined> => {
+    try {
+        if (userID || userID.trim() === '') {
+            throw new Error('Invalid userID for player one');
+        }
+
+        const groupDocRef = doc(db, 'groups', groupID)
+        const duelRef = doc(groupDocRef, 'duels', duelID)
+
+        await updateDoc(duelRef, {
+            [`usersWhoBet.${userID}`]: {
+                placedBet: true,
+                wager: wager,
+                createdAt: serverTimestamp(),
+            },
+        });
+        
+
+    } catch (error) {
+        console.error("addBet - Error adding bet:", error);
+        return undefined;
+
+    }
+}
