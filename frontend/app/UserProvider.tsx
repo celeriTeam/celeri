c9d3163051f4f532828c87cb764a97c51f383527
lchin <lukas.chin@msg.com>
2024-10-14 22:26:06 -0400

    attempting firebase listener

--- frontend/app/UserProvider.tsx
+++ frontend/app/UserProvider.tsx
@@ -1,37 +1,169 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { app } from "@firebaseConfig";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, collection, query, where, onSnapshot } from "firebase/firestore";
+import { getProfilePic, getUserName, getSteps, getUserGroups } from '@/backend/src/users';
+import { getGroupIDFromGroupName, getGroupName, getGroupCode, getGroupProfilePic, getGroupIsGameActive, getGroupIsFirstDay, getGroupCreator, getUserTokens, getTodaysBetTokens, getUsersInGroup, getDefaultBetOnSelf } from '@/backend/src/groups';
+import { getYesterdaysDuelsSummary, getTodaysDuelsSummary, getUnbetDuels, checkFinishedBetting, checkFinishedRecap } from '@/backend/src/bets';
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export interface UserContextType {
     userID: string;
+    profileImageUrl: string;
+    username: string;
+    steps: number;
+    groupNames: string[];
+    groups: { [groupID: string]: any };
     loading: boolean;
 }
 
-const UserContext = createContext<UserContextType>({ userID: '', loading: true });
+const UserContext = createContext<UserContextType>({
+    userID: '',
+    profileImageUrl: '',
+    username: '',
+    steps: 0,
+    groupNames: [],
+    groups: {},
+    loading: true,
+});
 
 export const useUser = () => useContext(UserContext);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [userID, setUserID] = useState<string>('');
+    const [profileImageUrl, setProfileImageUrl] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [steps, setSteps] = useState<number>(0);
+    const [groupNames, setGroupNames] = useState<any[]>([]);
+    const [groups, setGroups] = useState<{ [groupID: string]: any }>({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        let unsubscribeUser: any;
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setUserID(user.uid);
+                unsubscribeUser = fetchUserData(user.uid);
             } else {
                 setUserID('');
+                clearUserData();
             }
             setLoading(false);
         });
 
-        return () => unsubscribe();
+        return () => {
+            unsubscribe(); // Cleanup the auth state listener
+            // Cleanup the user document snapshot listener
+            if (typeof unsubscribeUser === 'function') {
+                unsubscribeUser();
+            }
+        };
     }, []);
 
+    const fetchUserData = async (uid: string) => {
+        const unsubscribeUser = onSnapshot(doc(db, "users", uid), async (docSnapshot) => {
+            if (docSnapshot.exists()) {
+                const currentProfilePicUrl = await getProfilePic(uid);
+                setProfileImageUrl(currentProfilePicUrl || '');
+                const currentUsername = await getUserName(uid);
+                setUsername(currentUsername || '');
+                const currentUserSteps = await getSteps(uid);
+                setSteps(currentUserSteps || 0);
+
+                const userGroups = await getUserGroups(uid); // List of group names
+                if (userGroups) {
+                    setGroupNames(userGroups);
+                    fetchGroupData(userGroups, uid);
+                }
+            }
+        });
+
+        return unsubscribeUser;
+    };
+
+    const fetchGroupData = async (userGroups: string[], uid: string) => {
+        const groups: { [groupID: string]: any } = {};
+        if (userGroups) {
+            await Promise.all(userGroups.map(async (groupName) => {
+                const groupID = await getGroupIDFromGroupName(groupName);
+                const groupsRef = collection(db, "groups");
+                const groupDocRef = doc(groupsRef, groupID);
+                const unsubscribeGroup = onSnapshot(groupDocRef, async (docSnapshot) => {
+                    if (docSnapshot.exists() && groupID) {
+                        const [groupCode, groupImageUrl, groupName, isGameActive, isFirstDay, groupCreator, userTokens, defaultBetOnSelf, todaysBetTokens, yesterdaysDuels, todaysDuels, unbetDuels, isFinishedBetting, isFinishedRecap] = await Promise.all([
+                            getGroupCode(groupID),
+                            getGroupProfilePic(groupID),
+                            getGroupName(groupID),
+                            getGroupIsGameActive(groupID),
+                            getGroupIsFirstDay(groupID),
+                            getGroupCreator(groupID),
+                            getUserTokens(groupID, uid),
+                            getDefaultBetOnSelf(groupID),
+                            getTodaysBetTokens(groupID, uid),
+                            getYesterdaysDuelsSummary(groupID),
+                            getTodaysDuelsSummary(groupID),
+                            getUnbetDuels(groupID, uid),
+                            checkFinishedBetting(groupID, uid),
+                            checkFinishedRecap(groupID, uid)
+                        ]);
+
+                        const userList = await getUsersInGroup(groupID); // userIDs
+                        const users: { [userID: string]: any } = {};
+                        if (userList) {
+                            await Promise.all(userList.map(async (selectedUserID) => {
+                                const [profilePic, username, steps, tokens] = await Promise.all([
+                                    getProfilePic(selectedUserID),
+                                    getUserName(selectedUserID),
+                                    getSteps(selectedUserID),
+                                    getUserTokens(groupID, selectedUserID)
+                                ]);
+                        
+                                users[selectedUserID] = {
+                                    profilePic,
+                                    username,
+                                    steps,
+                                    tokens
+                                };
+                            }));
+                        }
+                        groups[groupID] = {
+                            groupCode,
+                            groupImageUrl,
+                            groupName,
+                            isGameActive,
+                            isFirstDay,
+                            groupCreator,
+                            userTokens,
+                            defaultBetOnSelf,
+                            todaysBetTokens,
+                            yesterdaysDuels,
+                            todaysDuels,
+                            unbetDuels,
+                            isFinishedBetting,
+                            isFinishedRecap,
+                            userList,
+                            users
+                        };
+                    }
+                });
+                return unsubscribeGroup;
+            }));
+        }
+        setGroups(groups);
+    };
+
+    const clearUserData = () => {
+        setProfileImageUrl('');
+        setUsername('');
+        setSteps(0);
+        setGroupNames([]);
+        setGroups({});
+    };
+
     return (
-        <UserContext.Provider value={{ userID, loading }}>
+        <UserContext.Provider value={{ userID, profileImageUrl, username, steps, groupNames, groups, loading }}>
             {children}
         </UserContext.Provider>
     );
