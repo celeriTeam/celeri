47f54d844928fd8ef0f2ebaab7acec8bcb83fbbd
aidancng <aidancng@gmail.com>
2025-07-21 18:05:17 -0400

    fixed development build

---
+++ frontend/react-native.config.js
@@ -0,0 +1,7 @@
+// react-native.config.js
+module.exports = {
+  dependencies: {
+    // this pulls in all of expo-dev-client’s native bits
+    ...require('expo-dev-client/dependencies'),
+  },
+};
