f201b461e59c123077a909c8f5b70e7a19cb297a
Lukas Chin <lukaschin000@gmail.com>
2025-08-19 15:48:18 -0400

    fixed comp results page bug (referrals not showing)

--- frontend/backend/src/api.ts
+++ frontend/backend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const databaseUrl = 'https://celeri.onrender.com'; // or your dev URL
-// const databaseUrl = `http://10.168.89.219:3000`; // for local development: 'IPv4 Address' when you run 'ipconfig' in terminal
+// const databaseUrl = 'https://celeri.onrender.com'; // or your dev URL
+const databaseUrl = `http://192.168.1.249:3000`; // for local development: 'IPv4 Address' when you run 'ipconfig' in terminal
 
 export const api = axios.create({
     baseURL: databaseUrl,
