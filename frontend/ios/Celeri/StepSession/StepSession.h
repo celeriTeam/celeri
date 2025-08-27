de497fba72956bdea8908dd8877778532c9e2c43
aidancng <aidancng@gmail.com>
2025-08-26 23:25:56 -0400

    added files

---
+++ frontend/ios/Celeri/StepSession/StepSession.h
@@ -0,0 +1,6 @@
+// // ios/<YourApp>/StepSession/StepSession.h
+// #import <React/RCTEventEmitter.h>
+// #import <React/RCTBridgeModule.h>
+
+// @interface StepSession : RCTEventEmitter <RCTBridgeModule>
+// @end
---
+++ frontend/modules/live-healthkit/build/LiveHealthkitView.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { LiveHealthkitViewProps } from './LiveHealthkit.types';
+export default function LiveHealthkitView(props: LiveHealthkitViewProps): React.JSX.Element;
+//# sourceMappingURL=LiveHealthkitView.d.ts.map
